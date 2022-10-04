47a82c44cab6016021e52f38f993e1cbbff3b56c
luboslenco <lubos.lenco@gmail.com>
2022-10-04 17:59:50 +0200

    Add sd convert script

--- train/stable_diffusion/to_onnx.py
+++ train/stable_diffusion/to_onnx.py
@@ -1,4 +1,6 @@
 
-if __name__ == '__main__':
-    from onnxruntime.quantization.quantize import quantize_dynamic, QuantType
-    quantized_model = quantize_dynamic("CompVis/stable-diffusion-v1-4_onnx/unet/model.onnx", "sd_unet.onnx", weight_type=QuantType.QInt8)
+from onnxruntime.quantization.quantize import quantize_dynamic, QuantType
+quantized_model = quantize_dynamic("stable_diffusion_onnx/unet/model.onnx", "sd_unet.quant.onnx", weight_type=QuantType.QInt8)
+quantized_model = quantize_dynamic("stable_diffusion_onnx/text_encoder/model.onnx", "sd_text_encoder.quant.onnx", weight_type=QuantType.QInt8)
+quantized_model = quantize_dynamic("stable_diffusion_onnx/vae_decoder/model.onnx", "sd_vae_decoder.quant.onnx", weight_type=QuantType.QInt8)
+quantized_model = quantize_dynamic("stable_diffusion_onnx/vae_encoder/model.onnx", "sd_vae_encoder.quant.onnx", weight_type=QuantType.QInt8)
