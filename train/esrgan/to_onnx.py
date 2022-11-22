11ad8f81bec6aee142953bd220f266ab29e68e87
luboslenco <lubos.lenco@gmail.com>
2022-11-22 18:43:38 +0100

    Quantize with quint8

--- train/esrgan/to_onnx.py
+++ train/esrgan/to_onnx.py
@@ -6,12 +6,12 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
 model.load_state_dict(torch.load('RealESRGAN_x2plus.pth')['params_ema']) # 'params'
 model.train(False)
-model.cuda().eval()
+model.eval()
 
-x = torch.rand(1, 3, 64, 64).cuda()
+x = torch.rand(1, 3, 64, 64)
 dynamic_axes= {'data':{2:'width', 3:'height'}, 'output':{2:'width', 3:'height'}}
 with torch.no_grad():
-    torch_out = torch.onnx._export(model, x, 'model.onnx', input_names=['data'], output_names=['output'], dynamic_axes=dynamic_axes, opset_version=15)
+    torch_out = torch.onnx._export(model, x, 'esrgan.onnx', input_names=['data'], output_names=['output'], dynamic_axes=dynamic_axes, opset_version=15)
 
 from onnxruntime.quantization.quantize import quantize_dynamic, QuantType
-quantized_model = quantize_dynamic("model.onnx", "model.quant.onnx", weight_type=QuantType.QInt8)
+quantized_model = quantize_dynamic("esrgan.onnx", "esrgan.quant.onnx", weight_type=QuantType.QUInt8)
